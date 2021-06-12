650273cd5ff41826aa0c999f8c6fc62de77add73
vincentcollis <vinsanity0050@gmail.com>
2021-06-12 17:39:28 -0400

    added card values

---
+++ src/components/Cards.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const CardButton = styled.button`
+    background-color: green;
+    margin: 5px;
+`
+
+
+function Cards() {
+
+    // save card count values
+    let two = {name: "2", value:1}
+    let three = {name: "3", value:1}
+    let four = {name: "4", value:1}
+    let five = {name: "5", value:1}
+    let six = {name: "6", value:1}
+    let seven = {name: "7", value:0}
+    let eight = {name: "8", value:0}
+    let nine = {name: "9", value:0}
+    let ten = {name: "10", value:-1}
+    let jack = {name: "Jack", value:-1}
+    let queen = {name: "Queen", value:-1}
+    let king = {name: "King", value:-1}
+    let ace = {name: "Ace", value:-1}
+
+    // save all cards to on object array
+    let card = [two,three,four,five,six,seven,eight,nine,ten,jack,queen,king,ace]
+
+    //test buttons work
+    const printValue = 
+    return (
+        <>
+            {card.map((obj, i) => 
+                <CardButton key={i} data-value={obj.value}> {obj.name} </CardButton>)
+            }
+        </>
+    )
+}
+
+export default (Cards);
