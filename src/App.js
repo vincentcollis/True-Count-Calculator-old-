b5dc974e489568f5326808ea2cc69f6c7bdc9b49
vincentcollis <vinsanity0050@gmail.com>
2021-06-12 18:50:15 -0400

    shoesize component done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import Cards from './components/Cards.js'
 import RunningCount from './components/RunningCount';
+import ShoeSize from './components/ShoeSize';
 
 function App() {
 
@@ -12,14 +13,22 @@ function App() {
   const [runningCount, setrunningCount] = useState(0)
   const [deckPen, setdeckPen] = useState(0)
   const [trueCount, setTrueCount] = useState(0)
+  const [shoeSize, setShoeSize] = useState(0)
   
   return (
     <div className="App">
-      <div>Cards</div>
-      <Cards setRunningCount = {setrunningCount} runningCount = {runningCount}></Cards>
       <div>
-        Running Count: <RunningCount runningCount = {runningCount}></RunningCount>
+        Shoe Size:
       </div>
+      <ShoeSize setShoeSize = {setShoeSize}/>
+      <div>
+        Cards
+      </div>
+      <Cards setRunningCount = {setrunningCount} runningCount = {runningCount}/>
+      <div>
+        Running Count:
+      </div>
+      <RunningCount runningCount = {runningCount}/>
       <div>
         Deck Penertration
       </div>  
